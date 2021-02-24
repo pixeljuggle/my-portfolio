6defb7071ce187c344d7b37787e42bee1ba29475
Alex White <pixeljuggle@gmail.com>
2021-02-24 17:25:32 +0000

    Refactoring clear form function

--- script.js
+++ script.js
@@ -14,11 +14,12 @@ But when i do i will use this info:
  " ${chat} "`);
 });
 
-reset.addEventListener('click', function (event) {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let chat = document.getElementById("chat").value;
-  name.value = '';
-  email.value = '';
-  chat.value = '';
+reset.addEventListener("click", function (event) {
+  var contactForm = document.getElementById("contact-me").elements;
+
+  for (var i = 0; i < contactForm.length; i++) {
+    if (contactForm[i].type != "submit" && contactForm[i].type != "reset") {
+      contactForm[i].value = "";
+    }
+  }
 });
