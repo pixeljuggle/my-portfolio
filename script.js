3340deaf7c55dd142bf077d0b77140b1ab15af93
Alex White <pixeljuggle@gmail.com>
2021-02-24 16:59:03 +0000

    Adding JS to contact form.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('helloooooo')
+let submit = document.getElementById("submit");
+
+let reset = document.getElementById("reset");
+
+submit.addEventListener('click', function (event) {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let chat = document.getElementById("chat").value;
+  alert(`I dont know how to submit forms right now!
+But when i do i will use this info:
+
+ ${name}
+ ${email}
+ " ${chat} "`);
+});
+
+reset.addEventListener('click', function (event) {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let chat = document.getElementById("chat").value;
+  name.value = '';
+  email.value = '';
+  chat.value = '';
+});
