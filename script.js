35c8da495259380bf44ddb1ba7ef7c6adbe00eab
Alex White <pixeljuggle@gmail.com>
2021-02-27 15:30:37 +0000

    Working version non persistant dark mode. Move away from sass

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+        // function to set a given theme/color-scheme
+        setTheme('theme-light');
+        function setTheme(themeName) {
+          localStorage.setItem('theme', themeName);
+          document.documentElement.className = themeName;
+      }
+
+      // function to toggle between light and dark theme
+      function toggleTheme() {
+          if (localStorage.getItem('theme') === 'theme-dark') {
+              setTheme('theme-light');
+          } else {
+              setTheme('theme-dark');
+          }
+      }
+
+      // Immediately invoked function to set the theme on initial load
+      (function () {
+          if (localStorage.getItem('theme') === 'theme-dark') {
+              setTheme('theme-dark');
+          } else {
+              setTheme('theme-light');
+          }
+      })();
+
 function lightDark() {
-  var main = document.body;
-  main.classList.toggle("body-dark");
+  
+  toggleTheme();
   
   var logo = document.getElementById('logo').src.includes("-dark");
 
@@ -20,4 +45,8 @@ else {
   document.getElementById('dark-mode').src ='./assets/img/aw-circle-dark.svg';
 }
 
+
 }
+
+
+
