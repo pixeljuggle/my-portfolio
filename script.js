276b76ce256a0d792494118f9cf841c05e83334a
Alex White <pixeljuggle@gmail.com>
2021-02-27 23:26:58 +0000

    Minor improvements to svg usage

--- script.js
+++ script.js
@@ -7,23 +7,15 @@ function setTheme(themeName) {
 function toggleTheme() {
   if (localStorage.getItem("theme") === "theme-light") {
     setTheme("theme-dark");
-    document.getElementById("logo").src = "./assets/img/aw-logo-dark.svg";
-    document.getElementById("dark-mode").src ="./assets/img/aw-circle-dark.svg";
   } else {
     setTheme("theme-light");
-    document.getElementById("logo").src = "./assets/img/aw-logo.svg";
-    document.getElementById("dark-mode").src = "./assets/img/aw-circle.svg";
   }
 }
 
 (function () {
   if (localStorage.getItem("theme") === "theme-dark") {
     setTheme("theme-dark");
-    document.getElementById("logo").src = "./assets/img/aw-logo-dark.svg";
-    document.getElementById("dark-mode").src ="./assets/img/aw-circle-dark.svg";
   } else {
     setTheme("theme-light");
-    document.getElementById("logo").src = "./assets/img/aw-logo.svg";
-    document.getElementById("dark-mode").src = "./assets/img/aw-circle.svg";
   }
 })();
