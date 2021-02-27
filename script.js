9156292742ccde71e9423433ada9a37612a00b12
Alex White <pixeljuggle@gmail.com>
2021-02-27 21:05:21 +0000

    Adding improvements to dark mode switch

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
-// function to set a given theme/color-scheme
-setTheme("theme-light");
+
 function setTheme(themeName) {
   localStorage.setItem("theme", themeName);
   document.documentElement.className = themeName;
 }
 
-// function to toggle between light and dark theme
 function toggleTheme() {
-  if (localStorage.getItem("theme") === "theme-dark") {
-    setTheme("theme-light");
-  } else {
+  if (localStorage.getItem("theme") === "theme-light") {
     setTheme("theme-dark");
+    document.getElementById("logo").src = "./assets/img/aw-logo-dark.svg";
+    document.getElementById("dark-mode").src ="./assets/img/aw-circle-dark.svg";
+  } else {
+    setTheme("theme-light");
+    document.getElementById("logo").src = "./assets/img/aw-logo.svg";
+    document.getElementById("dark-mode").src = "./assets/img/aw-circle.svg";
   }
 }
 
-// Immediately invoked function to set the theme on initial load
 (function () {
   if (localStorage.getItem("theme") === "theme-dark") {
     setTheme("theme-dark");
+    document.getElementById("logo").src = "./assets/img/aw-logo-dark.svg";
+    document.getElementById("dark-mode").src ="./assets/img/aw-circle-dark.svg";
   } else {
     setTheme("theme-light");
+    document.getElementById("logo").src = "./assets/img/aw-logo.svg";
+    document.getElementById("dark-mode").src = "./assets/img/aw-circle.svg";
   }
 })();
-
-function lightDark() {
-  toggleTheme();
-
-  var logo = document.getElementById("logo").src.includes("-dark");
-
-  if (logo) {
-    document.getElementById("logo").src = "./assets/img/aw-logo.svg";
-  } else {
-    document.getElementById("logo").src = "./assets/img/aw-logo-dark.svg";
-  }
-
-  var darkMode = document.getElementById("dark-mode").src.includes("-dark");
-
-  if (darkMode) {
-    document.getElementById("dark-mode").src = "./assets/img/aw-circle.svg";
-  } else {
-    document.getElementById("dark-mode").src =
-      "./assets/img/aw-circle-dark.svg";
-  }
-}
